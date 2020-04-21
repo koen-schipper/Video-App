6b5ab197b127902c70a3e20b862ba4f496e3fa2f
Koen Schipper <koen.schipper@me.com>
2020-04-21 10:35:42 +0200

    Final commit

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
+const VideoList = ({ videos, onVideoSelect }) => {
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
+    });
+
     return (
-        <div>
-            {props.videos.length}
+        <div className="ui relaxed divided list">
+            {renderedList}
         </div>
     );
 };
